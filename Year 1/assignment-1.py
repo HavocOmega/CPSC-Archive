d68f8c2f559e699eb91d6dd9fbf36ceaa921a3b1
HavocOmega <80192800+HavocOmega@users.noreply.github.com>
2024-09-17 00:39:05 -0600

    Update assignment-1.py

--- Year 1/assignment-1.py
+++ Year 1/assignment-1.py
@@ -1,3 +1,6 @@
+# Assignment 1
+# Written by: Ryll
+
 # Create a hashtable of valid string responses
 valid_responses = {}
 valid_responses["is_citizen"] = ["Yes", "yes", "no", "No"]
@@ -5,6 +8,11 @@ valid_responses["is_resident"] = ["Yes", "yes", "no", "No"]
 valid_responses["birth_month"] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 valid_responses["birth_day"] = []
 valid_responses["birth_year"] = []
+
+# Months with 30 days
+thirty_day_months = ["April", "June", "September", "November"]
+# Months with 31 days
+thirty_one_day_months = ["January", "March", "May", "July", "August", "October", "December"]
 
 # Dictionary of all wanted problems
 questions = {
@@ -18,19 +26,52 @@ questions = {
 # Hash table of responses to questions to store for later (tbh idk why i store them it's just convenient for later)
 stored_responses = {}
 
-def isResponseValid(value, hashList):
+def isResponseValid(question, value):
     if value.isdigit():
-        print("isDigit")
-    elif not value in hashList:
+        intValue = int(value)
+        match question:
+            case 'birth_day':
+                birth_month = stored_responses["birth_month"]
+                
+                match birth_month:
+                    case birth_month if birth_month in thirty_day_months:
+                        if intValue > 30:
+                            print("Invalid birth date.")
+                            return 2;
+                    case birth_month if birth_month in thirty_one_day_months:
+                        if intValue > 31:
+                            print("Invalid birth date.")
+                            return 2;
+                    case _:
+                        if intValue > 29:
+                            print("Invalid birth date.")
+                            return 2;
+                        
+            case 'birth_year':
+                if intValue < 1900 or intValue > 2024:
+                    print("Invalid birth date.")
+                    return 2;
+            case _:
+                print("Invalid response.")
+                return 1;
+    elif not value in valid_responses.get(question):
         print("Invalid response.")
-        quit()
+        return 1;
+    return 0
 
 def askQuestions():
     # Loop through questions dictionary
     for i, v in questions.items():
         # Store responses by the index from the questions dictionary
         stored_responses[i] = input(v + " ")
-        isResponseValid(stored_responses[i], valid_responses.get(i))
+        isValid = isResponseValid(i, stored_responses[i])
+        if isValid > 0:
+            print("EXIT CODE: " + str(isValid))
+            quit(isValid)
+            
+def initialize():
+    askQuestions()
+    
 
 # Initialize program
-askQuestions()
+initialize()
