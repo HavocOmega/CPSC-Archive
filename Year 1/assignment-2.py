ac6cac4b3077405666d35eb94e94f4f47ae585e5
HavocOmega <80192800+HavocOmega@users.noreply.github.com>
2024-10-17 12:35:21 -0600

    Update assignment-2.py

--- Year 1/assignment-2.py
+++ Year 1/assignment-2.py
@@ -9,7 +9,7 @@ fertility_data = {}
 # Loop until the inputted amount of data points is greater than or equal to 2 data points
 while amtOfDataPoints < 2:
     data_points_input = input("How many data points do you have? ")
-    if int(data_points_input) >= 2: #Check if input is greater than or wual to 2
+    if int(data_points_input) >= 2: #Check if input is greater than or equal to 2
         amtOfDataPoints = int(data_points_input)
     else:
         print("Must enter at least two data points.")
@@ -22,22 +22,22 @@ for i in range(int(amtOfDataPoints)):
     fertility_data[year_input] = fertility_input #Store fertility data by the year in an array (Using an array is super convenient because it allows to override pre-existing values wihtout any extra work)
 
 #Nested if statement hell
-start_year = input("Which year would you like to start with? ")
-if fertility_data[start_year]:
-    end_year = input("Which year would you like to end with? ")
-    if fertility_data[end_year]:
-        if int(end_year) > int(start_year):
-            avg_fertility = (float(fertility_data[start_year]) + float(fertility_data[end_year])) / 2
-            print("The average fertility rate of these two years is %.2f." % float(avg_fertility))
-            if float(fertility_data[start_year]) > float(fertility_data[end_year]):
+start_year = input("Which year would you like to start with? ") #Query for start year
+if fertility_data[start_year]: #Check if the input is valid
+    end_year = input("Which year would you like to end with? ") #Query for the end year
+    if fertility_data[end_year]: #Check if the input is valid
+        if int(end_year) > int(start_year): #Check if end year is after start year
+            avg_fertility = (float(fertility_data[start_year]) + float(fertility_data[end_year])) / 2 #Calculate average fertility
+            print("The average fertility rate of these two years is %.2f." % float(avg_fertility)) #Print average fertility
+            if float(fertility_data[start_year]) > float(fertility_data[end_year]): #Check if the fertility data for the start year is greater than the end year
                 print("There is a downward trend.") 
-            elif float(fertility_data[start_year]) < float(fertility_data[end_year]):
+            elif float(fertility_data[start_year]) < float(fertility_data[end_year]): #Check if the fertility data for the start year is less than the end year
                 print("There is an upward trend.") 
-            else:
+            else: #If trend is neither upward nor downward then it's sideways
                 print("There is a sideways trend.") 
         else:
             print("End year must be after start year.")
     else:
-        print("The start year does not exist.")
+        print("The end year does not exist.")
 else:
     print("The start year does not exist.")
